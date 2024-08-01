1bb14f352b62dc16f3319bb96810827d629f6214
Alaeddine Ben Mahmoud <alaeddine.benmahmoud@esprit.tn>
2024-08-01 19:54:39 +0100

    347. Top K Frequent Elements (01/08/2024)

--- Arrays && Hashing/347. Top K Frequent Elements.java
+++ Arrays && Hashing/347. Top K Frequent Elements.java
@@ -8,3 +8,44 @@
 
 // Input: nums = [1], k = 1
 // Output: [1]
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        
+        //Create a HashMap to count occurrences
+        //In a Java HashMap, the structure is {key: value}
+
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i: nums){
+            int currentCount= map.getOrDefault(i, 0);
+            int newCount = currentCount+1;
+            map.put(i,newCount);
+        }
+
+        PriorityQueue <Map.Entry<Integer,Integer>> pq =
+            new PriorityQueue<>( (a,b) -> b.getValue()- a.getValue());
+        //The comparator (a, b) -> a.getValue() - b.getValue() ensures that the PriorityQueue maintains a min-heap based on the counts (occurrences) of the numbers.
+        //The entry with the smallest count will always be at the root of the heap.
+        
+        // Iterate over each entry in the map
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            pq.add(entry); // Add the current entry to pq
+        }
+        // Extract the top K numbers from the priority queue
+        int[] result = new int [k];
+        // int index =0;
+        // while(!pq.isEmpty()){
+        //     result[index++] = pq.poll().getKey();
+        // }
+        for(int i=0;i<k;i++){
+            result[i]= pq.poll().getKey();
+        }
+        return result;
+    }
+}
