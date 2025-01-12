4859b169105af8ca8021dc723f7ff969a6d4d66d
Alaeddine Ben Mahmoud <alaeddine.benmahmoud@esprit.tn>
2025-01-12 22:24:36 +0100

    1207. Unique Number of Occurrences   (easy)

---
+++ Hash Map&&Set/1207. Unique Number of Occurrences.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : arr) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+        Set<Integer> occurrenceSet = new HashSet<>(countMap.values());
+        return occurrenceSet.size() == countMap.size();
+    }
+}
