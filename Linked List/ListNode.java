044f6153cb11694679206e2e22bfa09d13fd5c93
Alaeddine Ben Mahmoud <alaeddine.benmahmoud@esprit.tn>
2025-01-19 19:12:34 +0100

    206. Reverse Linked List (easy)

---
+++ Linked List/ListNode.java
@@ -0,0 +1,15 @@
+// Definition for singly-linked list.
+public class ListNode {
+    int val;
+    ListNode next;
+
+    // Constructors
+    ListNode() {}
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
